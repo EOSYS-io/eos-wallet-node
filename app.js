d14186fb67dee5706d84b59780ed6e6150da2d1c
Sinhyeok Seo <sinhyeok@chain.partners>
2018-07-05 17:33:27 +0900

    Use bios-boot-tutorial.py

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ if (process.env.NODE_ENV === 'production') {
     Config.httpEndpoint = 'https://rpc.eosys.io'
 }
 else {
-    Config.chainId = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
-    Config.keyProvider = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
-    Config.httpEndpoint = 'http://127.0.0.1:8888'
+    Config.chainId = '1c6ae7719a2a3b4ecb19584a30ff510ba1b6ded86e1fd8b8fc22f1179c622a32'
+    Config.keyProvider = '5K463ynhZoCDDa4RDcr63cUwWLTnKqmdcoTKTHBjqoKfv4u5V7p'
+    Config.httpEndpoint = 'http://127.0.0.1:8000'
 }
 
 app.get('/', function(req, res) {
@@ -59,30 +59,34 @@ app.get('/account', function(req, res) {
 app.post('/account', jsonParser, function(req, res) {
     eos = Eos(Config)
     
+    eosAccount = creator()
     accountName = req.body.account_name
     pubkey = req.body.pubkey
     
     eos.transaction(tr => {
         tr.newaccount({
-            creator: creator(),
+            creator: eosAccount,
             name: accountName,
             owner: pubkey,
             active: pubkey
-        })
+        }
+        )
         
         tr.buyrambytes({
-            payer: creator(),
+            payer: eosAccount,
             receiver: accountName,
             bytes: 8192
-        })
+        }
+        )
         
         tr.delegatebw({
-            from: creator(),
+            from: eosAccount,
             receiver: accountName,
-            stake_net_quantity: '0.1000 EOS',
-            stake_cpu_quantity: '0.1000 EOS',
+            stake_net_quantity: '100.0000 SYS',
+            stake_cpu_quantity: '100.0000 SYS',
             transfer: 0
-        })
+        }
+        )
     }).then(function(result) {
         res.send(result)
     }, function(error) {
