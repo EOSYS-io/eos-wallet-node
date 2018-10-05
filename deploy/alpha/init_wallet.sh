eea4aa67c95cae3595781d22edba706465fcc04a
Sinhyeok Seo <sinhyeok@chain.partners>
2018-10-06 00:53:46 +0900

    Fix keosd api endpoint port

--- deploy/alpha/init_wallet.sh
+++ deploy/alpha/init_wallet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EOS_NODE_URL=https://rpc.eosys.io
-KEOSD_URL=http://127.0.0.1:8888
+KEOSD_URL=http://127.0.0.1:8900
 
 # Wait until keosd is loaded
 loaded=false
@@ -10,8 +10,10 @@ while [ $loaded != true ]; do
     response=$(curl -s -o /dev/null -I -w "%{http_code}" $KEOSD_URL/v1/wallet/list_wallets)
     if [[ $response =~ $regex ]]; then
       loaded=true
+      echo "############ keosd loaded ############"
     else
       sleep 5
+      echo "waiting keosd loading....."
     fi
 done
 
